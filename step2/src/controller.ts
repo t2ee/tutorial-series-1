57906cc2f47748815e0e140af3bfe11e84335174
Qiaosen Huang <joesonw@gmail.com>
2017-08-04 21:22:20 +0800

    added step2

---
+++ step2/src/controller.ts
@@ -0,0 +1,107 @@
+import {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    Consumes,
+    Body,
+    Path,
+    Response,
+    PathParam,
+    QueryParam,
+} from '@t2ee/vader';
+import {
+    Component,
+} from '@t2ee/core';
+
+let LIST = [{
+    id: 0,
+    name: 'a',
+}, {
+    id: 1,
+    name: 'b',
+}];
+
+@Path('/todo')
+@Component
+export default class Controller {
+    @GET
+    @Path('/')
+    async getList(@QueryParam('id') id): Promise<Response> {
+        if (id) {
+            id = parseInt(id);
+            const item = LIST.find(todo => todo.id === id);
+            if (item) {
+                const response = new Response();
+                response.body = item;
+                return response;    
+            }
+        } else {
+            const response = new Response();
+            response.body = LIST;
+            return response;
+        }
+    }
+
+    @GET
+    @Path('/:id')
+    async getItem(@PathParam('id') id) {
+        id = parseInt(id);
+        const item = LIST.find(todo => todo.id === id);
+        if (item) {
+            const response = new Response();
+            response.body = item;
+            return response;    
+        }
+    }
+
+    @Consumes('application/json')
+    @POST
+    @Path('/:id?')
+    async postItem(@Body body, @PathParam('id') id) {
+        if (!id) {
+            id = LIST.length;
+        } else {
+            id = parseInt(id);
+        }
+        const item = {
+            id,
+            name: body.name,
+        };
+        LIST.push(item);
+        const response = new Response();
+        response.body = item;
+        return response;
+    }
+
+    @Consumes('application/json')
+    @PUT
+    @Path('/:id?')
+    async putItem(@Body body, @PathParam('id') id) {
+        if (!id) {
+            id = body.id;
+        } else {
+            id = parseInt(id);
+        }
+        const item = LIST.find(todo => todo.id === id);
+        if (item) {
+            item.name = body.name;
+            const response = new Response();
+            response.body = item;
+            return response;
+        }
+    } 
+   
+    @DELETE
+    @Path('/:id')
+    async deleteItem(@PathParam('id') id) {
+        id = parseInt(id);
+        const index = LIST.findIndex(todo => todo.id === id);
+        if (index >= 0) {
+            LIST.splice(index, 1);
+            const response = new Response();
+            response.body = 'OK';
+            return response;
+        }
+    } 
+}
